19c294b3ab89a493b1c21f9e1c88eea39d1e1cc2
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-04 20:33:01 +0700

    feat: add primary key, required, string length, and auto increment to user

---
+++ Aplikasi Apotek/Models/Log.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aplikasi_Apotek.Models
+{
+    public class Log
+    {
+        public int Id_log { get; set; }
+        public DateTime Waktu { get; set; }
+        [StringLength(50)]
+        public string Aktivitas { get; set; }
+        public int Id_user { get; set; }
+    }
+}
--- Aplikasi Apotek/Models/User.cs
+++ Aplikasi Apotek/Models/User.cs
@@ -1,20 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Aplikasi_Apotek.Models
 {
     public class User
     {
+        
+        [Key] // Primary Key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Auto Increment
+        [Required] // Membuat Field Id_user Menjadi Required
         public int Id_user { get; set; }
+
+        [Required]
         [StringLength(50)]
         public string Tipe_user { get; set; }
+
+        [Required]
         [StringLength(50)]
         public string Nama { get; set; }
+
+        [Required]
         [StringLength(150)]
         public string Alamat { get; set; }
+
+        [Required]
         [StringLength(50)]
         public string Telpon { get; set; }
+
+        [Required]
         [StringLength(50)]
         public string Username { get; set; }
+
         [StringLength(50)]
         public string? Password { get; set; } = null;
     }
