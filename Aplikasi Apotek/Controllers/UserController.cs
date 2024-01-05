c952b53e600532bb98eb20dd9363210c8b6c89e1
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-05 23:26:54 +0700

    feat: api get data user

---
+++ Aplikasi Apotek/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Aplikasi_Apotek.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplikasi_Apotek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApotekContext _apotekContext;
+
+        public UserController(ApotekContext apotekContext)
+        {
+            _apotekContext = apotekContext;
+        }
+
+        // Get All User Data
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            if(_apotekContext.User == null)
+            {
+                return NotFound();
+            }
+            return await _apotekContext.User.ToListAsync();
+        }
+    }
+}
